e2a473f9ebd01bdff5ba34e025377590ef979194
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-08-31 16:25:51 +0300

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Author SL = new Author("Stanislav", "Lem");
-        Book Solaris = new Book("Solaris", 1961, SL);
-        System.out.println("Book: " + Solaris.getName() + "; Year of publication: " + Solaris.getPublishingYear() + "; Author: " + Solaris.getAuthor().getFirstName() + " " + Solaris.getAuthor().getSecondName());
+        //Book Solaris = new Book("Solaris", 1961, SL);
+        //System.out.println("Book: " + Solaris.getName() + "; Year of publication: " + Solaris.getPublishingYear() + "; Author: " + Solaris.getAuthor().getFirstName() + " " + Solaris.getAuthor().getSecondName());
         Author RB = new Author("Ray", "Bradbury");
-        Book Fahrenheit = new Book("Fahrenheit 451", 1953, RB);
-        System.out.println("Book: " + Fahrenheit.getName() + "; Year of publication: " + Fahrenheit.getPublishingYear() + "; Author: " + Fahrenheit.getAuthor().getFirstName() + Fahrenheit.getAuthor().getSecondName());
+        //Book Fahrenheit = new Book("Fahrenheit 451", 1953, RB);
+        //System.out.println("Book: " + Fahrenheit.getName() + "; Year of publication: " + Fahrenheit.getPublishingYear() + "; Author: " + Fahrenheit.getAuthor().getFirstName() + Fahrenheit.getAuthor().getSecondName());
         System.out.println(new Book("Solaris", 1961, SL));
+        System.out.println(new Book("Fahrenheit 451", 1953, RB));
         //Fahrenheit.setPublishingYear(1953);
         //System.out.println("Corrected. Year of publication: " + Fahrenheit.getPublishingYear());
     }
