aff8ae2065e1abbf28981ea904b9f71b52e499ce
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-08-31 16:47:16 +0300

    Added four (two new ones) books

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,21 @@ public class Main {
         Author RB = new Author("Ray", "Bradbury");
         //Book Fahrenheit = new Book("Fahrenheit 451", 1953, RB);
         //System.out.println("Book: " + Fahrenheit.getName() + "; Year of publication: " + Fahrenheit.getPublishingYear() + "; Author: " + Fahrenheit.getAuthor().getFirstName() + Fahrenheit.getAuthor().getSecondName());
-        System.out.println(new Book("Solaris", 1961, SL));
-        System.out.println(new Book("Fahrenheit 451", 1953, RB));
+        //System.out.println(new Book("Solaris", 1961, SL));
+        //System.out.println(new Book("Fahrenheit 451", 1953, RB));
         //Fahrenheit.setPublishingYear(1953);
         //System.out.println("Corrected. Year of publication: " + Fahrenheit.getPublishingYear());
+        Book Solaris = new Book("Solaris", 1961, SL);
+        String Book1 = Solaris.toString();
+        System.out.println(Book1);
+        Book Fahrenheit451 = new Book("Fahrenheit 451", 1953, RB);
+        String Book2 = Fahrenheit451.toString();
+        System.out.println(Book2);
+        Book MagellanicCloud = new Book("The Magellanic Cloud", 1955, SL);
+        String Book3 = MagellanicCloud.toString();
+        System.out.println(Book3);
+        Book DandelionWine = new Book("Dandelion Wine", 1957, RB);
+        String Book4 = DandelionWine.toString();
+        System.out.println(Book4);
     }
 }
